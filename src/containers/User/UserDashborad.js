7a6f4f617adf9b468e87e7254a7428ffc95147db
amit036 <amit68803@gmail.com>
2020-07-12 16:46:49 +0530

    Added: Test Rank Module

--- src/containers/User/UserDashborad.js
+++ src/containers/User/UserDashborad.js
@@ -1,22 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import http from 'axios'
 import '../../css/LandingPage.css'
 import '../../css/DashBoard.css';
 import UserNavBar from '../User/UserNavBar'
+
+const USER_ID = 2
 const Dashboard = () => {
-  
-   return(
- 
+
+  const [mockPapers, setMockTestPaper] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    http
+      .get('http://localhost:5000/getAllMockPaper', {
+        headers: {
+          user_id: USER_ID
+        }
+      })
+      .then(res => {
+        setLoading(false)
+        setMockTestPaper(res.data.mock_paper)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  return (
+
     <div >
-       <UserNavBar />
-       <div className="container text-center py-5">
+      <UserNavBar />
+      <div className="container text-center py-5">
         <div className="info-header mb-5">
-            <h1 className="text-primary pb-3">
-              Test Will Start From 15th July
-            </h1>         
-          </div>        
-        </div>   
+          <h1 className="text-primary pb-3">
+            Test Will Start From 15th July
+            </h1>
+        </div>
+      </div>
+      {
+        !loading ?
+          <div class='table-responsive col-10 offset-1 text-center'>
+            <table id="tablePreview" class="table table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>S.No</th>
+                  <th>Paper Name</th>
+                  <th>Paper Date</th>
+                  <th>Status</th>
+                  <th>View Details</th>
+                  <th className="text-primary" style={{ fontSize: '16px', fontStyle: 'italic' }}>*Result will released next day of exam</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  mockPapers.map((data, index) => {
+                    return (
+                      <tr>
+                        <th scope="row">{index + 1}</th>
+                        <td className="font-weight-bold">{data.mock_paper_name}</td>
+                        <td>{data.paper_date}</td>
+                        {
+                          !data.is_active && !data.is_finished ?
+                            <td className="text-primary font-weight-bold">Upcoming</td> :
+                            data.is_active && !data.is_finished ?
+                              <td className="text-success font-weight-bold blink">Live&#8226; </td> :
+                              !data.is_active && data.is_finished ?
+                                <td className="text-danger font-weight-bold">Finished</td> :
+                                null
+                        }
+
+                        <td><button className="btn btn-secondary">Click Here</button></td>
+
+                        {
+                          !data.is_active && data.is_finished && data.is_attempted && data.is_result_released ?
+                            <td>
+                              <button className="btn btn-success mr-2">View Result</button>
+                              <button className="btn btn-danger">View Rank</button>
+                            </td>
+                            :
+                            !data.is_active && data.is_finished && !data.is_attempted ?
+                              <td>
+                                <button className="btn btn-success mr-2" disabled>Not Attempted</button>
+                              </td>
+                              :
+                              data.is_active && !data.is_finished && data.is_attempted ?
+                                <td><button className="btn btn-danger" disabled>Completed</button></td>
+                                :
+                                data.is_active && !data.is_finished ?
+                                  <td><button className="btn btn-primary">Start Test</button></td>
+                                  :
+                                  !data.is_active && !data.is_finished ?
+                                    <td><button className="btn btn-primary" disabled>Coming Soon</button></td>
+                                    :
+                                    null
+                        }
+
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+          :
+          <div>Loading...</div>
+      }
     </div>
-    
-   )
+
+  )
 };
 export default Dashboard;
---
+++ src/pages/TestSeries/TestRank.js
@@ -0,0 +1,165 @@
+import React from 'react'
+
+var rankData = [
+    {
+        "user_id":3,
+        "user_firstname":"Nitu",
+        "user_lastname":"Kumari",
+        "user_email":"nitu@gmail.com",
+        "marks":87,
+        "accuracy":89,
+        "paper_time":3400
+    },
+    {
+        "user_id":6,
+        "user_firstname":"Ashish",
+        "user_lastname":"Kumar",
+        "user_email":"ashish@gmail.com",
+        "marks":85,
+        "accuracy":79,
+        "paper_time":3600
+    },
+    {
+        "user_id":4,
+        "user_firstname":"Amit",
+        "user_lastname":"Kumar",
+        "user_email":"amit@gmail.com",
+        "marks":77,
+        "accuracy":89,
+        "paper_time":3500
+    },
+    {
+        "user_id":8,
+        "user_firstname":"Suraj",
+        "user_lastname":"Kumar",
+        "user_email":"suraj@gmail.com",
+        "marks":75.6,
+        "accuracy":89,
+        "paper_time":3800
+    },
+    {
+        "user_id":10,
+        "user_firstname":"Aman",
+        "user_lastname":"Kumar",
+        "user_email":"aman@gmail.com",
+        "marks":75,
+        "accuracy":69,
+        "paper_time":3500
+    },
+    {
+        "user_id":1,
+        "user_firstname":"Madhu",
+        "user_lastname":"Kumar",
+        "user_email":"madhu@gmail.com",
+        "marks":74.2,
+        "accuracy":82,
+        "paper_time":3200
+    },
+    {
+        "user_id":18,
+        "user_firstname":"Atul",
+        "user_lastname":"Kumar",
+        "user_email":"atul@gmail.com",
+        "marks":73,
+        "accuracy":76,
+        "paper_time":3900
+    },
+    {
+        "user_id":25,
+        "user_firstname":"Piyush",
+        "user_lastname":"Kumar",
+        "user_email":"piyush@gmail.com",
+        "marks":71.5,
+        "accuracy":72,
+        "paper_time":3540
+    },
+    {
+        "user_id":14,
+        "user_firstname":"Tanveer",
+        "user_lastname":"Kumar",
+        "user_email":"tanveer@gmail.com",
+        "marks":71.2,
+        "accuracy":65,
+        "paper_time":4200
+    },
+    {
+        "user_id":19,
+        "user_firstname":"Rohit",
+        "user_lastname":"Kumar",
+        "user_email":"rohit@gmail.com",
+        "marks":70.5,
+        "accuracy":90,
+        "paper_time":4200
+    }
+
+]
+
+function fancyTimeFormat(time)
+    {   
+        var hrs = ~~(time / 3600);
+        var mins = ~~((time % 3600) / 60);
+        var secs = ~~time % 60;
+        var ret = "";
+
+        if (hrs > 0) {
+            ret += "" + hrs + "h " + (mins < 10 ? "0" : "");
+        }
+
+        ret += "" + mins + "m " + (secs < 10 ? "0" : "");
+        ret += "" + secs + "s";
+        return ret;
+    }
+
+const USER_ID = 4
+// const MOCK_PAPER_ID = 2
+
+export default function TestRank() {
+
+    return (
+        <div className="col-8 offset-2 text-center">
+            <table className="table table-hover table-striped border border-secondary">
+            <thead className="bg-dark text-white">
+                <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Marks</th>
+                <th>Accuracy</th>
+                <th>Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    rankData.map((data,index)=>{
+                        if(USER_ID === data.user_id){
+                            return(
+                                <tr id={data.id} className="bg-success text-white shadow p-3">
+                                    <th scope="row">#{index+1}</th>
+                                    <td>You</td>
+                                    <td>{data.user_email}</td>
+                                    <td>{data.marks}</td>
+                                    <td>{data.accuracy}%</td>
+                                    <td>{fancyTimeFormat(data.paper_time)}</td>
+                            </tr>
+                            )
+                        }else{
+                            return(
+                                <tr id={data.id}>
+                                    <th scope="row">#{index+1}</th>
+                                    <td>{data.user_firstname} {data.user_lastname}</td>
+                                    <td>{data.user_email}</td>
+                                    <td>{data.marks}</td>
+                                    <td>{data.accuracy}%</td>
+                                    <td>{fancyTimeFormat(data.paper_time)}</td>
+                                </tr>
+                            )
+                        }
+
+                    })
+                }
+                
+            </tbody>
+            </table>
+        </div>
+    )
+}
