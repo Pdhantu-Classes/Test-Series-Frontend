0b31d400c9251863867b16e7ac3c7018cee587b8
amit036 <amit68803@gmail.com>
2020-08-16 10:08:09 +0530

    Changes

--- src/containers/Course/DashboardCourse/index.js
+++ src/containers/Course/DashboardCourse/index.js
@@ -67,8 +67,7 @@ const UserDashBoard = () => {
               </div>
   
               <div
-                className="col-lg-4 col-md-6 mt-5"
-                onClick={() => {handleChange(2)}}>
+                className="col-lg-4 col-md-6 mt-5">
                 <div className="card bg-success ">
                   <div className="card-body py-5" style={{ height: "35vh" }}>
                   <div className="display-4 py-4 text-white">Pdhantu Class Test</div>
@@ -76,8 +75,7 @@ const UserDashBoard = () => {
                 </div>
               </div>
               <div
-                className="col-lg-4 col-md-6  mt-5"
-                onClick={() => {handleChange(3)}}>
+                className="col-lg-4 col-md-6  mt-5">
                 <div className="card bg-danger ">
                   <div className="card-body py-5" style={{ height: "35vh" }}>
                     <div className="py-5 text-white" style={{fontSize:"45px"}}>Assignments</div>
