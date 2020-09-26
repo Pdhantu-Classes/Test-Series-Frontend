df30e2dfbc4492ebf96ccf362c2c7b9848ca999b
amit036 <amit68803@gmail.com>
2020-09-27 00:52:53 +0530

    Added: CGPSC TestSeries Module

--- src/pages/LandingPage/Components/TestSeriesSchedule.js
+++ src/pages/LandingPage/Components/TestSeriesSchedule.js
@@ -4,7 +4,7 @@ export default function TestSeriesSchedule() {
     return (
         <div className="text-center">
             <div className="text-danger display-4">Test Schedule </div>
-            <img className=" img-fluid rounded mx-auto d-block w-50" src="https://pdhantu-classes.s3.us-east-2.amazonaws.com/miscellaneous/cgpsc-course.jpeg" alt="CGPSC-DETAILS"/>
+            <img className=" img-fluid rounded mx-auto d-block w-50" src="https://pdhantu-classes.s3.us-east-2.amazonaws.com/miscellaneous/testseries-schedule.jpeg" alt="CGPSC-DETAILS"/>
         </div>
     )
 }
